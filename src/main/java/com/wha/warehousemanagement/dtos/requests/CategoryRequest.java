6a02e9943226298c30fe47f0243e9254eb6f08ac
hieuphung41 <phungduchieu6996@gmail.com>
2024-05-14 21:38:11 +0700

    add request folder and response folder

---
+++ src/main/java/com/wha/warehousemanagement/dtos/requests/CategoryRequest.java
@@ -0,0 +1,20 @@
+package com.wha.warehousemanagement.dtos.requests;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CategoryRequest {
+    private Integer id;
+
+    private String name;
+
+    private String description;
+}
