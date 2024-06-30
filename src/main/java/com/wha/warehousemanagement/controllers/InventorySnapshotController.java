c4dd883bfff16d557cc9a3c64d959af643521caa
TaiNgo30 <ngoductai2004@gmail.com>
2024-06-30 17:09:48 +0700

    change API path inventory snapshot

--- src/main/java/com/wha/warehousemanagement/controllers/InventorySnapshotController.java
+++ src/main/java/com/wha/warehousemanagement/controllers/InventorySnapshotController.java
@@ -18,7 +18,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/inventory/snapshot")
+@RequestMapping("/inventory-snapshot")
 @RequiredArgsConstructor
 public class InventorySnapshotController {
     private final InventorySnapshotService inventorySnapshotService;
